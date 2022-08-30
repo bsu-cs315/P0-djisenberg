2a44f9bba587bd5b72325a3e7d73f555222c32b0
Daniel <djisenberg2015@gmail.com>
2022-08-30 15:01:54 -0400

    Add HUD scene and implemented UI controls

--- Main.gd
+++ Main.gd
@@ -7,7 +7,7 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
+	#new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,11 +18,15 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_StartTimer_timeout():
@@ -32,6 +36,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_MobTimer_timeout():
